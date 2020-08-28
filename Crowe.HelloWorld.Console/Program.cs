9ff23b13a6dc67e59c6556741ef896563303a147
depersioa <depersioa@reninc.com>
2020-08-27 21:40:18 -0400

    Initial commit

---
+++ Crowe.HelloWorld.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace Crowe.HelloWorld.Console
+{
+    private class Program
+    {
+        public static IConfigurationRoot configuration;
+        
+        private static void Main(string[] args)
+        {
+            LoadConfiguration();
+            System.Console.WriteLine(configuration.GetSection("BaseUrl").Value);
+            System.Console.WriteLine("Hello World!");
+        }
+
+        private static void LoadConfiguration()
+        {
+            //Load appsettings.json
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
