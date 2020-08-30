aef5443f51de087a8d5f8d0f849efce34d0cace3
depersioa <depersioa@reninc.com>
2020-08-30 00:27:38 -0400

    Added some unit tests

---
+++ Crowe.HelloWorld.Test/Api/Controllers/HelloWorldControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Crowe.HelloWorld.Api.Controllers;
+using Crowe.HelloWorld.Writers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crowe.HelloWorld.Test.Api.Controllers
+{
+    [TestClass]
+    public class HelloWorldControllerTest
+    {
+        private IWriter _writer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _writer = new ConsoleWriter();
+        }
+
+        [TestMethod]
+        public void WriteTest()
+        {
+            var helloWorldController = new HelloWorldController(_writer);
+            var response = helloWorldController.Write();
+            var statusCode = ((StatusCodeResult) response).StatusCode;
+
+            Assert.IsTrue(statusCode == 200);
+        }
+    }
+}
---
+++ Crowe.HelloWorld.Test/Writers/ConsoleWriterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crowe.HelloWorld.Writers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crowe.HelloWorld.Test.Writers
+{
+    [TestClass]
+    public class ConsoleWriterTest
+    {
+        [TestMethod]
+        public void WriteTest()
+        {
+            //Just make sure we don't throw any exceptions
+            try
+            {
+                var consoleWriter = new ConsoleWriter();
+                consoleWriter.Write("Hello World");
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Exception Occurred: {ex.Message}");
+            }
+        }
+    }
+}
