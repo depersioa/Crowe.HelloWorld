5b4cc30e7a12933b1f97360d7f5ab81f5d229c1a
depersioa <depersioa@reninc.com>
2020-08-29 22:46:24 -0400

    Writting out hello world from the api response

--- Crowe.HelloWorld.Console/Program.cs
+++ Crowe.HelloWorld.Console/Program.cs
@@ -2,18 +2,48 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Crowe.HelloWorld.Models.Response;
 
 namespace Crowe.HelloWorld.Console
 {
     class Program
     {
         public static IConfigurationRoot configuration;
-        
+
+        private static readonly HttpClient client = new HttpClient();
+
         private static void Main(string[] args)
         {
             LoadConfiguration();
-            System.Console.WriteLine(configuration.GetSection("BaseUrl").Value);
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine(GetHelloWorld());
+        }
+
+        private static string GetHelloWorld()
+        {
+            try
+            {
+                var url = configuration.GetSection("BaseUrl").Value + "/api/helloworld";
+
+                var responseTask = client.GetStringAsync(url);
+                var response = responseTask.Result;
+                var options = new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var helloWorldResponse = JsonSerializer.Deserialize<HelloWorldResponse>(response, options);
+
+                return helloWorldResponse.Data.Message;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
 
         private static void LoadConfiguration()
